f4a04815e481b578d689041df678a15919ff4b4f
24amishra <144934356+24amishra@users.noreply.github.com>
2025-01-11 15:25:21 -0500

    Update visual.py

--- visual.py
+++ visual.py
@@ -11,29 +11,73 @@ import numpy as np
 yearData = pd.read_csv('/Users/agastyamishra/Downloads/US-National-Parks_RecreationVisits_1979-2023.csv')
 
 monthData = pd.read_csv('/Users/agastyamishra/Downloads/US-National-Parks_Use_1979-2023_By-Month.csv')
-intermountain = yearData[yearData['Region']=='Intermountain']
-intermountain = intermountain.groupby(['ParkName'],as_index=False).sum()
-print(intermountain)
+#intermountain = yearData[yearData['Region']=='Intermountain']
+#intermountain = intermountain.groupby(['ParkName'],as_index=False).sum()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#average of all the data in monthData per year
+monthData = monthData.groupby(['Year','Region'],as_index=False).sum()
 
 
 #  aggregate data so only one category(total vis) is visible for each year
 yearData = yearData.groupby(['Year', 'Region'], as_index=False).sum()
-
-""" app = Dash()
-
+app = Dash()
 
 
 
 
 
 
-app.layout = [
-    html.Div(children='Total Recreation Visits by Region'),
-    dcc.Graph(figure=px.line(yearData, x='Year', y='RecreationVisits', color = 'Region'))
-]
+
+app.layout = html.Div([
+       
+    
+    html.Div(children='An interdisciplinary analysis of the National Parks System'),
+    dcc.Graph(figure=px.line(yearData, x='Year', y='RecreationVisits', color = 'Region')),
+    dcc.Graph(figure = px.line(monthData, x = 'Year', y = 'TentCampers', color = 'Region' )),
+
+    html.Div([
+            dcc.Dropdown(
+                
+                'Number of RV Campers',
+                id='crossfilter-yaxis-column'
+            ),
+])
+
+])
+
+
+
+
+
+
+
+
+
+
+"""  @callback(
+    Output('crossfilter-indicator-scatter', 'figure'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'),
+    Input('crossfilter-yaxis-type', 'value'),
+    Input('crossfilter-year--slider', 'value')) """
 
 
 #run file 
 if __name__ == '__main__':
     app.run(debug=True)
- """
